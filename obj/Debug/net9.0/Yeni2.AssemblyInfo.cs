293efe5bf0da0076546c983a1c56fbeb028dd974
MangoSiddeeq <mangosiddeeq@gmail.com>
2025-06-19 23:16:58 +0300

    added an 1-10 jackpot game

--- obj/Debug/net9.0/Yeni2.AssemblyInfo.cs
+++ obj/Debug/net9.0/Yeni2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Yeni2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d844a527ac6aa7f1a331f07ed52071fc6518ab2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bb0113a0168e25b5c5878ea851c0f1ead450197")]
 [assembly: System.Reflection.AssemblyProductAttribute("Yeni2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yeni2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- program1.cs
+++ program1.cs
@@ -12,15 +12,16 @@ namespace Yeni2;
 */
 
 //Explicit
+/*
 class Program1
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Sayı Girermisin ? ");
+        Console.WriteLine("Ondalıklı bir sayı girermisin ? ");
         double x = Convert.ToDouble(Console.ReadLine());
         int y = (int)Math.Round(x);
         bool? isXGreater;
-        if (Math.Round(x) == x)  
+        if (Math.Round(x) == x)
         {
             isXGreater = null;
         }
@@ -42,5 +43,34 @@ class Program1
             Console.WriteLine("Aşağı yuvarlanmış");
         }
         Console.WriteLine("Yuvarlanmış Olduğu Sayı " + y + " Çıkmıştır");
+    }
+}
+*/
+
+class Program1
+{
+    static void Main(string[] args)
+    {
+        Console.Write("1 ile 10 arasında bir sayı gir : ");
+        int girilenSayi = Convert.ToInt32(Console.ReadLine());
+        bool b = girilenSayi == 1 || girilenSayi == 2 || girilenSayi == 3 || girilenSayi == 4 || girilenSayi == 5 || girilenSayi == 6 || girilenSayi == 7 || girilenSayi == 8 || girilenSayi == 9 || girilenSayi == 10;
+        if (b)
+        {
+            bool dogruSayi = girilenSayi == 5 || girilenSayi == 7;
+            if (dogruSayi)
+            {
+                Console.WriteLine("JACKPOOOT");
+            }
+            else
+            {
+                Console.WriteLine("Yazııık Olduuu");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz Tekrar Dene");
+            Environment.Exit(0);
+        }
+
     }
 }
